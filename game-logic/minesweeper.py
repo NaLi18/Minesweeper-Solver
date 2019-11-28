92e00baad99d40853bd558bdf546afcc5a1254f8
Na_Li <NaLi18@github.com>
2019-11-28 07:45:35 -0500

    the changed for the minesweeper algorithm

--- game-logic/minesweeper.py
+++ game-logic/minesweeper.py
@@ -1,21 +1,11 @@
 from random import randint
 import numpy as np
-from queue import PriorityQueue
+from collections import defaultdict 
 
 def surrounding_cells(x, y, rows, cols):
-  x_0, x_1 = x-1, x+2
-  y_0, y_1 = y-1, y+2
-  if x_0 < 0:
-    x_0 += 1
-  elif x_1 > rows:
-    x_1 -= 1
-  if y_0 < 0:
-    y_0 += 1
-  elif y_1 > cols:
-    y_1 -= 1
   cells = []
-  for i in range(x_0, x_1):
-    for j in range(y_0, y_1):
+  for i in range(max(x-1, 0), min(x+2, rows)):
+    for j in range(max(y-1, 0), min(y+2, rows)):
       if i==x and j==y:
         continue
       cells.append([i, j])
@@ -37,28 +27,15 @@ def create_board(rows, cols, n):
 
 def display_state(state):
   rows, cols = len(state[0]), len(state[0][0])
-  rows_spacing, cols_spacing = len(str(rows)) + 1 +6, len(str(cols)) + 1 +6
+  rows_spacing, cols_spacing = len(str(rows)) + 1, len(str(cols)) + 1
   print('{:>{rs}}'.format(' ', rs=rows_spacing), end='')
   for i in range(cols):
-    print('{:>{cs}}'.format(i+1, cs=cols_spacing), end='')
+    print('{:>{cs}}'.format(i, cs=cols_spacing), end='')
   print('')
   for i in range(rows):
-    print('{:>{rs}}'.format(i+1, rs=rows_spacing), end='')
+    print('{:>{rs}}'.format(i, rs=rows_spacing), end='')
     for j in range(cols):
       print('{:>{cs}}'.format(state[0][i][j], cs = cols_spacing), end='')
-    print('')
-
-def display_prob(state):
-  rows, cols = len(state[0]), len(state[0][0])
-  rows_spacing, cols_spacing = len(str(rows)) + 1 +6, len(str(cols)) + 1 +6
-  print('{:>{rs}}'.format(' ', rs=rows_spacing), end='')
-  for i in range(cols):
-    print('{:>{cs}}'.format(i+1, cs=cols_spacing), end='')
-  print('')
-  for i in range(rows):
-    print('{:>{rs}}'.format(i+1, rs=rows_spacing), end='')
-    for j in range(cols):
-      print('{:>{cs}.2f}'.format(state[0][i][j], cs = cols_spacing), end='')
     print('')
 
 def play(row, col, state, board, flag=False):
@@ -72,9 +49,9 @@ def play(row, col, state, board, flag=False):
     if flag:
       state[2] += 1
     return True #not flag # game over only if mine clicked not flagged
-  # if flag:
-  #   print('No mine here!')
-  #   return False
+  if flag:
+    print('No mine here!')
+    return False
   queue = [[x, y]]
   while queue:
     x, y = queue.pop(0)
@@ -90,82 +67,70 @@ def play(row, col, state, board, flag=False):
     queue += surrounding_cells(x, y, rows, cols)
   return False
 
-def ai_player(state, probabilties, explored, first=True):
+def ai_player(state, board):
+  numbered_cell_neighbors = defaultdict(float)
   rows, cols = len(state[0]), len(state[0][0])
-  if first:
-    return randint(0, rows-1), randint(0, cols-1)
-  for i in range(rows):
-    for j in range(cols):
-      if state[0][i][j] == '':
-        explored[i][j] = 1
-        probabilties[i][j] = -100.0
-        for k, l in surrounding_cells(i, j, rows, cols):
-          probabilties[k][l] = -100.0
-      elif state[0][i][j].isdigit():
-        probabilties[i][j] = -100.0
-        n = int(state[0][i][j])
-        if explored[i][j] != 0:
-          continue
-        explored[i][j] = 1
-        cells = [[k, l] for k, l in surrounding_cells(i, j, rows, cols) if state[0][k][l] == '#']
-        for k, l in cells:
-          probabilties[k][l] += n/len(cells)
-  cell_prob = PriorityQueue()
-  cellp = []
-  for i in range(rows):
-    for j in range(cols):
-      if probabilties[i][j] > 0:
-        cell_prob.put([probabilties[i][j], (i, j)])
-        cellp.append([probabilties[i][j], (i, j)])
-  # print(cell_prob.get())
-  display_prob([probabilties])
-  print(cellp)
-  p, (x, y) = cell_prob.get()
-  print(p)
-  return x, y
+  # for i, j in itertools.product(range(rows), range(cols)):
+  for i, j in np.ndindex((rows, cols)):
+    if state[0][i][j].isdigit():
+      n = int(state[0][i][j])
+      flags = [(x, y) for x, y in surrounding_cells(i, j, rows, cols) if state[0][x][y] == 'F']
+      safe_factor = -20 if n == len(flags) else 1
+      neighbors = [(x, y) for x, y in surrounding_cells(i, j, rows, cols) if state[0][x][y] == '#']
+      if (n - len(flags)) == len(neighbors) and len(neighbors) > 0:
+        for x, y in neighbors:
+          play(x, y, state, board, True)
+        ai_player(state, board)
+      for x, y in neighbors:
+        numbered_cell_neighbors[(x, y)] += safe_factor * n/len(neighbors)
+  print(len(numbered_cell_neighbors), numbered_cell_neighbors)
+  if numbered_cell_neighbors:
+    x, y = min(numbered_cell_neighbors, key=numbered_cell_neighbors.get)
+    return x, y
+  print('Choosing random cell')
+  return randint(0, rows-1), randint(0, cols-1) # stuck, unable to choose next best cell
 
+random_fails = 0
 
-def main():
-  rows = int(input('Enter rows: '))
-  cols = int(input('Enter columns: '))
-  n = int(input('Enter number of mines: '))
+def autoplay(rows, cols, n):
+  global random_fails
   board, mines = create_board(rows, cols, n)
   num_mines = len(mines)
   display_state([board])
   print(mines)
   print('-'*30)
   state = [[['#']*cols for _ in range(rows)], 0, 0] # exposed grid, number of exposed cells, flagged mines
-  probabilties = np.zeros([rows, cols])
-  explored = np.zeros([rows, cols], dtype=int)
-  display_state(state)
   done = False
-  first = True
-  # flag = False
+  x, y = randint(0, rows-1), randint(0, cols-1)
+  is_random_fail = True
   while not done:
-    # print('Click on ')
-    try:
-      # x = int(input('\trow: '))
-      # y = int(input('\tcol: '))
-      x, y = ai_player(state, probabilties, explored, first)
-      first = False
-      print('Computer chose ', x+1, y+1)
-    except ValueError as _:
-      print('Invalid input!')
-      continue
-    done = play(x, y, state, board) #, flag)
+    print('Computer plays', (x, y), 'Mines flagged: ', state[2])
+    done = play(x, y, state, board)
     display_state(state)
-    if (rows*cols - state[1]) == num_mines or state[2] == num_mines:
-      print('Winner!')
-      done = True
-    elif not done:
-      # flag = False
-      option = input('Continue (enter \'N\' to quit)? ') #', \'F\' to flag)'
-      if option == 'N':
-        break
-      # if option == 'F':
-      #   flag = True
-    else:
+    if done:
+      if is_random_fail:
+        random_fails += 1
       print('Game Over!')
+      return False
+    if (rows*cols - state[1]) == num_mines:# or state[2] == num_mines:
+      print('Winner!')
+      return True
+    is_random_fail = False
+    x, y = ai_player(state, board)
+
+def main():
+  n = 1
+  wins = 0
+  losses = 0
+  for i in range(n):
+    if autoplay(10, 10, 10):
+      wins += 1
+    else:
+      losses += 1
+    if i>0: print(wins, losses, wins / i)
+  print(wins, losses, wins/n)
+  print('Game over at first try:', random_fails)
+  print('Excluding them, win percentage:', wins/(n-random_fails))
 
 if __name__ == '__main__':
   main()
